8afe0e4e29b5f86b6f7b3909afd0e97a2dd7dacb
CPapadim <papadimitriou.c@gmail.com>
2018-02-04 00:18:24 +0000

    Sync from DataScience rstudio

--- dashboard/app.R
+++ dashboard/app.R
@@ -50,7 +50,7 @@ get_data <- function() {
 
 predictions_all <- vector('numeric')
 liveish_data <- reactive({
-  invalidateLater(10)
+  invalidateLater(100)
   data_stream = get_data()
   model_predictions = get_prediction(data_stream, 100)
   predictions_all <<- c(predictions_all, model_predictions)
